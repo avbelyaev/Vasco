b2550c5c4c51c38d510da2ea8f0b79e51af12381
Anton Belyaev <anton.belyaev@bostongene.com>
2020-05-19 15:02:39 +0300

    desugar works!!!

--- sugar.rkt
+++ sugar.rkt
@@ -151,50 +151,55 @@
     [else                 (error (format "cannot desugar: ~s~n" def))]))
 
 
-; desugar : program -> program
+
 (define (desugar-program prog)
-
   (set! prog (tops-to-defs prog))
-  (displayln (pretty-format prog 40))
-  (displayln "-------------------")
-
   (set! prog (map desugar-define prog))
-  (displayln (pretty-format prog 40))
-  (displayln "-------------------")
-
   (set! prog
-    (partition-k
+    (partition
      atomic-define?
      prog
-     (λ (atomic complex)
-       (define bindings
+     (λ (variables complex)
+       (define func-declarations
          (for/list ([c complex])
            (match c
              [`(define ,v ,complex)         `(,v (void))])))
 
-       (define sets
+       (define function-definitions
          (for/list ([c complex])
            (match c
              [`(define ,v ,complex)         `(set! ,v ,complex)])))
 
-       (displayln "====")
-       (displayln (pretty-format atomic 40))
-       (displayln "====")
-       (displayln (pretty-format bindings 40))
-       (displayln "====")
-       (displayln (pretty-format sets 40))
-       (displayln "====")
-       (append atomic (list `(let ,bindings ,sets)))
+       (displayln "\n--------------- Simple variables ---------------")
+       (displayln (pretty-format variables 40))
+
+       (displayln "\n------------- Function declaration -------------")
+       (set! func-declarations (map function-declaration-mapper func-declarations))
+       (displayln (pretty-format func-declarations 40))
+
+       (displayln "\n------------- Function definitions -------------")
+       (displayln (pretty-format function-definitions 40))
+
+       (displayln "\n------------------- Result ---------------------")
+       (append
+            variables
+            func-declarations
+            function-definitions)
        )))
   prog)
 
 
 
-; partition-k : ('a -> boolean) 'a list ('a list 'a list -> 'a list 'a list)
-(define (partition-k pred lst k)
+(define (function-declaration-mapper exp)
+    (match exp
+        [`(,name ,voidBody)     `(define ,name ,voidBody)]
+        [else                   (error (format "mapper error: ~s\n" exp))]))
+
+
+(define (partition pred lst k)
   (if (not (pair? lst))
       (k '() '())
-      (partition-k pred (cdr lst) (λ (in out)
+      (partition pred (cdr lst) (λ (in out)
         (if (pred (car lst))
             (k (cons (car lst) in) out)
             (k in (cons (car lst) out)))))))
@@ -206,6 +211,15 @@
     (define (area x) (* pi 2 x x))
     (define pi 3.14)
     (display (area 10))
+    (define (foo x)
+        (cond
+            [(= x 0)  1]
+            [(> x 0)  (+ x (foo (- x 1)))]
+            [else     'universe-broke]))
+
+    (define bar 20)
+
+    (display (foo bar))
     ))
 
 (display (pretty-format (desugar-program test) 40))
